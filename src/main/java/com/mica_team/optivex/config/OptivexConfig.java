67a7cfb6150c034d3207ff8b3d29dbd6c38fa4c1
jimj90201-a11y <jimj90201@gmail.com>
2026-08-25 19:38:07 +0530

    Update OptivexConfig.java

--- src/main/java/com/mica_team/optivex/config/OptivexConfig.java
+++ src/main/java/com/mica_team/optivex/config/OptivexConfig.java
@@ -1,8 +1,6 @@
 package com.mica_team.optivex.config;
 
 import net.minecraftforge.common.ForgeConfigSpec;
-import net.minecraftforge.fml.ModLoadingContext;
-import net.minecraftforge.fml.config.ModConfig;
 
 public final class OptivexConfig {
 
@@ -12,22 +10,15 @@ public final class OptivexConfig {
     public static final ForgeConfigSpec.BooleanValue FAST_MATH =
             BUILDER
                     .comment(
-                            "Uses optimized trigonometric calculations.",
-                            "May slightly change mathematical precision."
+                            "Enables Optivex Fast Math.",
+                            "Uses optimized mathematical calculations.",
+                            "This option may slightly change floating-point precision."
                     )
                     .define("fast_math", false);
 
     public static final ForgeConfigSpec SPEC = BUILDER.build();
 
     private OptivexConfig() {
-    }
-
-    public static void load() {
-        ModLoadingContext.get().registerConfig(
-                ModConfig.Type.CLIENT,
-                SPEC,
-                "optivex-client.toml"
-        );
     }
 
     public static boolean isFastMathEnabled() {
