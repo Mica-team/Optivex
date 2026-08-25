191d9e49f2e9476fefc255a2e337d94923a4dcf1
jimj90201-a11y <jimj90201@gmail.com>
2026-08-25 19:50:03 +0530

    Update OptionsScreenMixin.java

--- src/main/java/com/mica_team/optivex/mixin/OptionsScreenMixin.java
+++ src/main/java/com/mica_team/optivex/mixin/OptionsScreenMixin.java
@@ -3,7 +3,9 @@ package com.mica_team.optivex.mixin;
 import com.mica_team.optivex.client.PerformanceScreen;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.screens.OptionsScreen;
+import net.minecraft.network.chat.Component;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -11,16 +13,20 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(OptionsScreen.class)
 public abstract class OptionsScreenMixin {
 
+    @Shadow
+    protected abstract <T extends net.minecraft.client.gui.components.events.GuiEventListener
+            & net.minecraft.client.gui.components.Renderable
+            & net.minecraft.client.gui.narration.NarratableEntry>
+    T addRenderableWidget(T widget);
+
     @Inject(method = "init", at = @At("TAIL"))
     private void optivex$addPerformanceButton(CallbackInfo ci) {
 
         OptionsScreen screen = (OptionsScreen) (Object) this;
 
-        screen.addRenderableWidget(
+        this.addRenderableWidget(
                 Button.builder(
-                        net.minecraft.network.chat.Component.literal(
-                                "Performance"
-                        ),
+                        Component.literal("Performance"),
                         button -> screen.getMinecraft().setScreen(
                                 new PerformanceScreen(screen)
                         )
@@ -32,4 +38,4 @@ public abstract class OptionsScreenMixin {
                 ).build()
         );
     }
-          }
+}
