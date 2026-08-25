02587e99065a789fc6a4aee90ba9f37306ad24b1
jimj90201-a11y <jimj90201@gmail.com>
2026-08-25 19:36:39 +0530

    Update Optivex.java

--- src/main/java/com/mica_team/optivex/Optivex.java
+++ src/main/java/com/mica_team/optivex/Optivex.java
@@ -1,11 +1,9 @@
 package com.mica_team.optivex;
 
-import com.mica_team.optivex.client.PerformanceScreen;
 import com.mica_team.optivex.config.OptivexConfig;
-import net.minecraftforge.client.ConfigScreenHandler;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 
 @Mod(Optivex.MOD_ID)
 public class Optivex {
@@ -13,15 +11,10 @@ public class Optivex {
     public static final String MOD_ID = "optivex";
 
     public Optivex() {
-        OptivexConfig.load();
-
-        ModLoadingContext.get().registerExtensionPoint(
-                ConfigScreenHandler.ConfigScreenFactory.class,
-                () -> new ConfigScreenHandler.ConfigScreenFactory(
-                        (minecraft, parent) -> new PerformanceScreen(parent)
-                )
+        ModLoadingContext.get().registerConfig(
+                ModConfig.Type.CLIENT,
+                OptivexConfig.SPEC,
+                "optivex-client.toml"
         );
-
-        MinecraftForge.EVENT_BUS.register(this);
     }
-          }
+}
