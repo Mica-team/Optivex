c2f86afb161f63e097044f6e5590749c0d72bf73
jimj90201-a11y <jimj90201@gmail.com>
2026-08-26 20:59:59 +0530

    Fix VideoSettingsScreen mixin protected widget access

--- src/main/java/com/mica_team/optivex/mixin/VideoSettingsScreenMixin.java
+++ src/main/java/com/mica_team/optivex/mixin/VideoSettingsScreenMixin.java
@@ -1,10 +1,12 @@
 package com.mica_team.optivex.mixin;
 
 import com.mica_team.optivex.client.PerformanceScreen;
+import net.minecraft.client.gui.components.AbstractWidget;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.screens.VideoSettingsScreen;
 import net.minecraft.network.chat.Component;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -12,13 +14,16 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(VideoSettingsScreen.class)
 public class VideoSettingsScreenMixin {
 
+    @Shadow
+    protected abstract <T extends net.minecraft.client.gui.components.events.GuiEventListener & net.minecraft.client.gui.components.Renderable & net.minecraft.client.gui.narration.NarratableEntry> T addRenderableWidget(T widget);
+
     @Inject(method = "init", at = @At("TAIL"))
     private void optivex$addPerformanceButton(CallbackInfo ci) {
         VideoSettingsScreen screen = (VideoSettingsScreen) (Object) this;
 
         System.out.println("[Optivex] Adding Performance button");
 
-        screen.addRenderableWidget(
+        addRenderableWidget(
             Button.builder(
                 Component.literal("Performance"),
                 button -> screen.getMinecraft().setScreen(
